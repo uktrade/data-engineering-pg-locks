2a612081337b5354c4c1c1d0de19c4be57de723e
nboyse <natasha.boyse@digital.trade.gov.uk>
2024-05-15 12:25:35 +0100

    task 2

--- hint_for_task_1.py
+++ hint_for_task_1.py
@@ -1,2 +1,9 @@
 if __name__ == "__main__":
-    print("Rollback needs to be added to line 49 for the first cursor")
+    print("cur_1.execute('ROLLBACK;')")
+
+    response = input("Do you need to know the line number? (Y/N): ")
+
+    if response.upper() == 'Y':
+        print("Implementation at line 49")
+    else:
+        print("Run this script again if you change your mind")
